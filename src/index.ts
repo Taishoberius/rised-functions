8e4f6e1b379d7e87d00efe179b9bafcd55929810
Cédric Nanteau <cedric.nanteau@gmail.com>
2019-07-16 17:37:13 +0200

    [ADD] send notif function

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ export const updPreference = functions.https.onRequest((req, res) => {
         "workAddress" : req.body.workAddress,
         "address" : req.body.address,
     }
-    
+
     return collection.doc(id).update(doc)
     .then(() => res.send(doc))
     .catch(err => res.status(500).send(err));
@@ -67,6 +67,42 @@ export const updPreference = functions.https.onRequest((req, res) => {
 // export const notification = functions.firestore.document('preferences/{id}').onWrite((change, context) => {
 
 // });
+
+export const sendNotification = functions.https.onRequest((req, res) => {
+    const id = req.query.id;
+    if (id == undefined) {
+        return res.status(300).send("Missing parameter id");
+    }
+    return collection.doc(id).get()
+        .then(snapshot => {
+            let data = snapshot.data();
+            if (data == undefined)
+                return res.status(400).send("ressource not found");
+
+            let token = data.token;
+            if (token == undefined || token == "") {
+                return res.status(400).send("no token");
+            }
+
+            const payload = {
+                data: {
+                    title: 'An event has occurred!',
+                    body: 'Please respond to this event.'
+                }
+            }
+
+            return admin.messaging().sendToDevice(token, payload)
+                .then(function(response) {
+                    console.log("Successfully sent message:", response);
+                    return res.status(200).send("Successfully sent notification");
+                })
+                .catch(function(error) {
+                    console.log("Error sending message:", error);
+                    return res.status(500).send("Failed to send notif");
+                });
+
+        })
+});
 
 const getDocuments = function(snap: FirebaseFirestore.QuerySnapshot) {
     const documents: any[] = [];
